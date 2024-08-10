f1e4ef15810235cbae6929a24c4e5bea36543d8b
Aniket <aniketraj28042003@gmail.com>
2024-08-10 07:08:38 +0530

    added fetch data for async thunk

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,8 +1,16 @@
 import MainData from "../Components/MainData/MainData";
 import SidebarData from "../Components/SidebarData/SidebarData";
 import RainImage from "../Assets/Bg/rain.webp";
+import { useEffect } from "react";
+import { fetchData } from "../Redux/ForecastSlice/ForecastSlice";
+import { useAppDispatch } from "../Hooks/hooks";
 
 export default function Home() {
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(fetchData());
+    }, []);
+
     return (
         <div className="flex justify-center items-stretch min-h-[100vh] px-4 py-4 text-black" style={{backgroundSize: '100%' ,backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${RainImage})`}}>
             <SidebarData/>
--- src/Redux/ForecastSlice/ForecastSlice.ts
+++ src/Redux/ForecastSlice/ForecastSlice.ts
@@ -1,16 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import ForecasteDataState from "../../Interface/ForecasteDataState";
+import axiosInstance from "../../Config/axiosInstance";
 
 const initialState : ForecasteDataState = {
     status: "default",
     data: undefined
 };
 
+export const fetchData = createAsyncThunk("data/fetchData", async () => {
+    try {
+        const response = await axiosInstance(`forecast.json?key=${import.meta.env.VITE_API_KEY}&q=Bengaluru&aqi=yes&days=7`);
+        // console.log(response);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 const forecasteSlice = createSlice({
     name: "forecaste",
     initialState,
     reducers: {},
-    extraReducers: () => {}
+    extraReducers: (builder) => {
+        builder.addCase(fetchData.fulfilled, (state, action) => {
+            if(!action.payload) return;
+            console.log(action);
+        })
+    }
 });
 
 export default forecasteSlice.reducer;
